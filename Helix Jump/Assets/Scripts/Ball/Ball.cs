b98e84b52145572cdef02e2a584c6f607689d9ae
LudmilaSavchenko <ludmila.sav.96@gmail.com>
2020-12-27 06:08:19 +0300

    Helix Jump

---
+++ Helix Jump/Assets/Scripts/Ball/Ball.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.TryGetComponent(out PlatformSegment platformSegment))
+        {
+            other.GetComponentInParent<Platform>().Break();
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.TryGetComponent(out PlatformSegment platformSegment))
+        {
+            if (platformSegment.GetComponentInParent<FinishPlatform>())
+            {
+                Debug.Log("Достигнута финишная платформа.");
+            }
+        }
+    }
+}
